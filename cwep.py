960cfd35996d9b04a9f419a9bd5d9b1196a2debc
ProVeg-Simon <37374038+ProVeg-Simon@users.noreply.github.com>
2018-07-30 18:32:35 +0200

    Add files via upload

---
+++ cwep.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import copy, sys
+import tkinter
+from tkinter import filedialog
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+root = tkinter.Tk()
+root.withdraw()
+filez = filedialog.askopenfilenames(parent=root,title='PDFs zum zusammenfügen auswählen',filetypes =(("PDFs", "*.pdf"),("All Files","*.*")))
+sf = filedialog.asksaveasfilename(parent=root,title='Speichern als',defaultextension=".pdf")
+output = PdfFileWriter()
+output_page_number = 0
+alignment = 2           # to align on even pages
+for filename in filez:
+    # This code is executed for every file in turn
+    input = PdfFileReader(open(filename, 'rb'))
+    for p in [input.getPage(i) for i in range(0,input.getNumPages())]:
+        # This code is executed for every input page in turn
+        output.addPage(p)
+        output_page_number += 1
+    while output_page_number % alignment != 0:
+        output.addBlankPage()
+        output_page_number += 1
+of = open(sf, 'bw')
+output.write(of)
